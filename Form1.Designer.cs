5114ce6e648f80811838c4ec06c1a33ec4b34064
KuaN32 <35547619+KuaN32@users.noreply.github.com>
2018-12-08 07:43:02 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.one = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.three = new System.Windows.Forms.Button();
+            this.four = new System.Windows.Forms.Button();
+            this.five = new System.Windows.Forms.Button();
+            this.six = new System.Windows.Forms.Button();
+            this.seven = new System.Windows.Forms.Button();
+            this.eight = new System.Windows.Forms.Button();
+            this.nine = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.display = new System.Windows.Forms.RichTextBox();
+            this.times = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.equals = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.backspace = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.decimals = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // one
+            // 
+            this.one.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.one.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.one.Location = new System.Drawing.Point(12, 382);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(101, 64);
+            this.one.TabIndex = 7;
+            this.one.TabStop = false;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = false;
+            this.one.Click += new System.EventHandler(this.one_Click);
+            // 
+            // two
+            // 
+            this.two.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.two.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.two.Location = new System.Drawing.Point(134, 382);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(101, 64);
+            this.two.TabIndex = 8;
+            this.two.TabStop = false;
+            this.two.Text = "2";
+            this.two.UseVisualStyleBackColor = false;
+            this.two.Click += new System.EventHandler(this.two_Click);
+            // 
+            // three
+            // 
+            this.three.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.three.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.three.Location = new System.Drawing.Point(258, 382);
+            this.three.Name = "three";
+            this.three.Size = new System.Drawing.Size(101, 64);
+            this.three.TabIndex = 9;
+            this.three.TabStop = false;
+            this.three.Text = "3";
+            this.three.UseVisualStyleBackColor = false;
+            this.three.Click += new System.EventHandler(this.three_Click);
+            // 
+            // four
+            // 
+            this.four.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.four.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.four.Location = new System.Drawing.Point(12, 315);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(101, 64);
+            this.four.TabIndex = 19;
+            this.four.TabStop = false;
+            this.four.Text = "4";
+            this.four.UseVisualStyleBackColor = false;
+            this.four.Click += new System.EventHandler(this.four_Click);
+            // 
+            // five
+            // 
+            this.five.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.five.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.five.Location = new System.Drawing.Point(134, 312);
+            this.five.Name = "five";
+            this.five.Size = new System.Drawing.Size(101, 64);
+            this.five.TabIndex = 20;
+            this.five.TabStop = false;
+            this.five.Text = "5";
+            this.five.UseVisualStyleBackColor = false;
+            this.five.Click += new System.EventHandler(this.five_Click);
+            // 
+            // six
+            // 
+            this.six.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.six.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.six.Location = new System.Drawing.Point(258, 312);
+            this.six.Name = "six";
+            this.six.Size = new System.Drawing.Size(101, 64);
+            this.six.TabIndex = 21;
+            this.six.TabStop = false;
+            this.six.Text = "6";
+            this.six.UseVisualStyleBackColor = false;
+            this.six.Click += new System.EventHandler(this.six_Click);
+            // 
+            // seven
+            // 
+            this.seven.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.seven.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seven.Location = new System.Drawing.Point(12, 245);
+            this.seven.Name = "seven";
+            this.seven.Size = new System.Drawing.Size(101, 64);
+            this.seven.TabIndex = 22;
+            this.seven.TabStop = false;
+            this.seven.Text = "7";
+            this.seven.UseVisualStyleBackColor = false;
+            this.seven.Click += new System.EventHandler(this.seven_Click);
+            // 
+            // eight
+            // 
+            this.eight.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.eight.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eight.Location = new System.Drawing.Point(134, 245);
+            this.eight.Name = "eight";
+            this.eight.Size = new System.Drawing.Size(101, 64);
+            this.eight.TabIndex = 23;
+            this.eight.TabStop = false;
+            this.eight.Text = "8";
+            this.eight.UseVisualStyleBackColor = false;
+            this.eight.Click += new System.EventHandler(this.eight_Click);
+            // 
+            // nine
+            // 
+            this.nine.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.nine.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nine.Location = new System.Drawing.Point(258, 245);
+            this.nine.Name = "nine";
+            this.nine.Size = new System.Drawing.Size(101, 64);
+            this.nine.TabIndex = 24;
+            this.nine.TabStop = false;
+            this.nine.Text = "9";
+            this.nine.UseVisualStyleBackColor = false;
+            this.nine.Click += new System.EventHandler(this.nine_Click);
+            // 
+            // zero
+            // 
+            this.zero.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.zero.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.zero.Location = new System.Drawing.Point(12, 452);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(223, 64);
+            this.zero.TabIndex = 25;
+            this.zero.TabStop = false;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = false;
+            this.zero.Click += new System.EventHandler(this.zero_Click);
+            // 
+            // display
+            // 
+            this.display.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.display.Font = new System.Drawing.Font("Papyrus", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.display.Location = new System.Drawing.Point(12, 12);
+            this.display.Name = "display";
+            this.display.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.display.Size = new System.Drawing.Size(519, 147);
+            this.display.TabIndex = 26;
+            this.display.TabStop = false;
+            this.display.Text = "";
+            // 
+            // times
+            // 
+            this.times.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.times.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.times.Location = new System.Drawing.Point(430, 245);
+            this.times.Name = "times";
+            this.times.Size = new System.Drawing.Size(101, 61);
+            this.times.TabIndex = 27;
+            this.times.TabStop = false;
+            this.times.Text = "*";
+            this.times.UseVisualStyleBackColor = false;
+            this.times.Click += new System.EventHandler(this.times_Click);
+            // 
+            // minus
+            // 
+            this.minus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.minus.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minus.Location = new System.Drawing.Point(430, 312);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(101, 64);
+            this.minus.TabIndex = 28;
+            this.minus.TabStop = false;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = false;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // plus
+            // 
+            this.plus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.plus.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plus.Location = new System.Drawing.Point(430, 382);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(101, 64);
+            this.plus.TabIndex = 29;
+            this.plus.TabStop = false;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = false;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // equals
+            // 
+            this.equals.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.equals.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.equals.Location = new System.Drawing.Point(365, 452);
+            this.equals.Name = "equals";
+            this.equals.Size = new System.Drawing.Size(166, 64);
+            this.equals.TabIndex = 30;
+            this.equals.TabStop = false;
+            this.equals.Text = "=";
+            this.equals.UseVisualStyleBackColor = false;
+            this.equals.Click += new System.EventHandler(this.equals_Click);
+            // 
+            // divide
+            // 
+            this.divide.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.divide.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divide.Location = new System.Drawing.Point(430, 178);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(101, 61);
+            this.divide.TabIndex = 33;
+            this.divide.TabStop = false;
+            this.divide.Text = "/";
+            this.divide.UseVisualStyleBackColor = false;
+            this.divide.Click += new System.EventHandler(this.divide_Click);
+            // 
+            // backspace
+            // 
+            this.backspace.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.backspace.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backspace.Location = new System.Drawing.Point(258, 178);
+            this.backspace.Name = "backspace";
+            this.backspace.Size = new System.Drawing.Size(101, 64);
+            this.backspace.TabIndex = 36;
+            this.backspace.TabStop = false;
+            this.backspace.Text = "<-";
+            this.backspace.UseVisualStyleBackColor = false;
+            this.backspace.Click += new System.EventHandler(this.backspace_Click);
+            // 
+            // clear
+            // 
+            this.clear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.clear.Font = new System.Drawing.Font("Papyrus", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear.Location = new System.Drawing.Point(134, 178);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(101, 64);
+            this.clear.TabIndex = 37;
+            this.clear.TabStop = false;
+            this.clear.Text = "CE";
+            this.clear.UseVisualStyleBackColor = false;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // decimals
+            // 
+            this.decimals.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.decimals.Font = new System.Drawing.Font("Papyrus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.decimals.Location = new System.Drawing.Point(258, 452);
+            this.decimals.Name = "decimals";
+            this.decimals.Size = new System.Drawing.Size(101, 64);
+            this.decimals.TabIndex = 38;
+            this.decimals.TabStop = false;
+            this.decimals.Text = ".";
+            this.decimals.UseMnemonic = false;
+            this.decimals.UseVisualStyleBackColor = false;
+            this.decimals.Click += new System.EventHandler(this.decimals_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(553, 525);
+            this.Controls.Add(this.decimals);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.backspace);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.equals);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.times);
+            this.Controls.Add(this.display);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.nine);
+            this.Controls.Add(this.eight);
+            this.Controls.Add(this.seven);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.three);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.one);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button two;
+        private System.Windows.Forms.Button three;
+        private System.Windows.Forms.Button four;
+        private System.Windows.Forms.Button five;
+        private System.Windows.Forms.Button six;
+        private System.Windows.Forms.Button seven;
+        private System.Windows.Forms.Button eight;
+        private System.Windows.Forms.Button nine;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.RichTextBox display;
+        private System.Windows.Forms.Button times;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button equals;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.Button backspace;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button decimals;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        //declaration of variables 
+        string input = string.Empty;        //store user input
+        string operand1 = string.Empty;     //store first operand
+        string operand2 = string.Empty;     //store second operand
+        char operation;
+        double result = 0.0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void zero_Click(object sender, EventArgs e)
+        {
+            this.display.Text = ""; //Clears the Text Box before the second input string is added
+            /* If the line above is not in here, typing a "6" 
+             * followed by a "9" would result in the display 
+             * box showing "669", and following it with a "2"
+             * would cause the display to show "669692"
+             */
+            input += "0";
+            this.display.Text += input;
+        }
+
+        private void one_Click(object sender, EventArgs e)
+        {
+            this.display.Text = "";
+            input += "1";
+            this.display.Text += input;
+        }
+
+        private void two_Click(object sender, EventArgs e)
+        {
+            this.display.Text = "";
+            input += "2";
+            this.display.Text += input;
+        }
+
+        private void three_Click(object sender, EventArgs e)
+        {
+            this.display.Text = "";
+            input += "3";
+            this.display.Text += input;
+        }
+
+        private void four_Click(object sender, EventArgs e)
+        {
+            this.display.Text = "";
+            input += "4";
+            this.display.Text += input;
+        }
+
+        private void five_Click(object sender, EventArgs e)
+        {
+            this.display.Text = "";
+            input += "5";
+            this.display.Text += input;
+        }
+
+        private void six_Click(object sender, EventArgs e)
+        {
+            this.display.Text = "";
+            input += "6";
+            this.display.Text += input;
+        }
+
+        private void seven_Click(object sender, EventArgs e)
+        {
+            this.display.Text = "";
+            input += "7";
+            this.display.Text += input;
+        }
+
+        private void eight_Click(object sender, EventArgs e)
+        {
+            this.display.Text = "";
+            input += "8";
+            this.display.Text += input;
+        }
+
+        private void nine_Click(object sender, EventArgs e)
+        {
+            this.display.Text = "";
+            input += "9";
+            this.display.Text += input;
+        }
+
+        private void decimals_Click(object sender, EventArgs e)
+        {
+            this.display.Text = "";
+            input += ".";
+            this.display.Text += input;
+        }
+
+        /*Need some sort of flag to store result as operand1 if
+         *the user wants to do some operation on the first result
+        */
+        private void plus_Click(object sender, EventArgs e)
+        {
+            operand1 = input;       //store input string into operand1
+            operation = '+';        //Set the operation to be '+'
+            input = string.Empty;   //Clear the input string to be able to receive the next value from the user
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            operand1 = input;       //store input string into operand1
+            operation = '-';        //Set the operation to be '-'
+            input = string.Empty;   //Clear the input string to be able to receive the next value from the user
+        }
+
+        private void times_Click(object sender, EventArgs e)
+        {
+            operand1 = input;       //store input string into operand1
+            operation = '*';        //Set the operation to be '+'
+            input = string.Empty;   //Clear the input string to be able to receive the next value from the user
+        }
+
+        private void divide_Click(object sender, EventArgs e)
+        {
+            operand1 = input;       //store input string into operand1
+            operation = '/';        //Set the operation to be '+'
+            input = string.Empty;   //Clear the input string to be able to receive the next value from the user
+        }
+
+        private void equals_Click(object sender, EventArgs e)
+        {
+            operand2 = input;
+            double num1, num2;
+            double.TryParse(operand1, out num1);
+            double.TryParse(operand2, out num2);
+
+            if (operation == '+')
+            {
+                result = num1 + num2;
+                this.display.Text = result.ToString();
+                operand1 = result.ToString();
+            }
+            else if (operation == '-')
+            {
+                result = num1 = num2;
+                this.display.Text = result.ToString();
+                operand1 = result.ToString();
+            }
+            else if (operation == '*')
+            {
+                result = num1 * num2;
+                this.display.Text = result.ToString();
+                operand1 = result.ToString();
+            }
+            else if (operation == '/')
+            {
+                if (num2 == 0)
+                {
+                    this.display.Text = "DIV/Zero!";
+                }
+                else
+                {
+                    result = num1 / num2;
+                    this.display.Text = result.ToString();
+                    operand1 = result.ToString();
+                }
+            }
+
+            
+        }
+
+        private void backspace_Click(object sender, EventArgs e)
+        {
+            this.display.Text = "";
+            input = input.Remove(input.Length - 1);
+            this.display.Text += input;
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            this.display.Text = "";
+            this.input = string.Empty;
+            this.operand1 = string.Empty;
+            this.operand2 = string.Empty;
+        }
+
+
+    }
+}
